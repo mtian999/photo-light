f36eaac2e1ce7720876744db6c96d83c8af5f4bf
tianbao <mtian999@163.com>
2024-11-14 12:17:52 +0800

    feat:删除一些

--- components/react-iztro/Iztrolabe/Iztrolabe.type.ts
+++
@@ -1,9 +0,0 @@
-import type { IztroInput } from "@/lib/hooks/iztro-hook/index.type";
-import { NestedProps } from "../config/types";
-
-export type IztrolabeProps = {
-  width?: number | string;
-  horoscopeDate?: string | Date;
-  horoscopeHour?: number;
-} & IztroInput &
-  NestedProps;
